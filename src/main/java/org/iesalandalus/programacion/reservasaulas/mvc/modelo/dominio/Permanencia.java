4583b48b4686a78f3515ca2f26e0808bcbe34061
ppcf_ <ppcf_@DESKTOP-8VRB9K0>
2022-03-07 21:03:12 +0100

    Quinto Commit
    Modificación clase Permanencia (abstract)

--- src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/dominio/Permanencia.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/dominio/Permanencia.java
@@ -2,21 +2,17 @@ package org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 
-public class Permanencia {
+// Edición clase abstracta
+public abstract class Permanencia {
 	
 	private LocalDate dia;
-	
-	// tramo (0,1)
-	private Tramo tramo;
 	
 	private static final DateTimeFormatter FORMATO_DIA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	
 	//Constructor con parametros
-	public Permanencia(LocalDate dia, Tramo tramo) {
-		setDia(dia);
-		setTramo(tramo);	
+	public Permanencia(LocalDate dia) {
+		setDia(dia);	
 	}
 	
 	//Constructor copia
@@ -25,7 +21,6 @@ public class Permanencia {
 			throw new NullPointerException("ERROR: No se puede copiar una permanencia nula.");		
 		}
 		setDia(permanencia.getDia());
-		setTramo(permanencia.getTramo());
 	}
 
 	public LocalDate getDia() {
@@ -40,41 +35,18 @@ public class Permanencia {
 		
 		this.dia = dia;
 	}
-	public Tramo getTramo() {
-		return tramo;
-	}
+	public abstract int getPuntos();
 	
-	private void setTramo(Tramo tramo) {
-		if (tramo== null){
-			throw new NullPointerException("ERROR: El tramo de una permanencia no puede ser nulo.");
-			
-		}
-		this.tramo= tramo;
-	}
-
-
 	@Override
-	public int hashCode() {
-		return Objects.hash(dia, tramo);
-	}
-
-
+	public abstract int hashCode();
+	
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Permanencia other = (Permanencia) obj;
-		return Objects.equals(dia, other.dia) && tramo == other.tramo;
-	}
-
-
+	public abstract boolean equals(Object obj);
+	
+	
 	@Override
 	public String toString() {
-		return "dia=" + getDia().format(FORMATO_DIA) + ", tramo=" + getTramo() ;
+		return "dia=" + getDia().format(FORMATO_DIA);
 	}
 	
 
